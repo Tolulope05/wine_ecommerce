6ac98e984a332d2d606c0aea0fe0424fb4795e93
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-09-26 17:37:27 +0100

    fix intro screen

---
+++ lib/view/intro_screen/boarding_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class BoardingItem extends StatelessWidget {
+  final Widget widget;
+  final String titleText;
+  final String subtitleText;
+  const BoardingItem({
+    Key? key,
+    required this.widget,
+    required this.titleText,
+    required this.subtitleText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                child: widget,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(35.0),
+                child: Column(
+                  children: [
+                    Text(
+                      subtitleText,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Color(0xff131313),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      titleText,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Color(0xff131313),
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/intro_screen/intro_screen.dart
+++ lib/view/intro_screen/intro_screen.dart
@@ -1,133 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wine_ecommerce/database/consts.dart';
+import 'package:wine_ecommerce/view/intro_screen/boarding_item.dart';
 import 'package:wine_ecommerce/view_model/intro_screen_model.dart';
 import 'package:wine_ecommerce/widgets/button.dart';
 
-class IntroScreen extends StatelessWidget {
+class IntroScreen extends StatefulWidget {
   const IntroScreen({Key? key}) : super(key: key);
 
   @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  int currentIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    IntroScreenModel introProvider = Provider.of<IntroScreenModel>(
-      context,
-    );
-    PageController introPagecontroller = PageController();
+    IntroScreenModel introProvider = Provider.of<IntroScreenModel>(context);
     return Scaffold(
-        body: PageView.builder(
-      controller: introPagecontroller,
-      itemBuilder: ((context, index) {
-        return SafeArea(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.1,
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: TextButton(
-                      onPressed: () {
-                        introProvider.goHome(context);
-                      },
-                      child: const Text(
-                        "Skip",
-                        style: TextStyle(
-                          color: greyColor,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Container(
-                padding: const EdgeInsets.all(20),
-                child: introProvider.getImage(introProvider.svgIcons[index]),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    child: Text(
-                      introProvider.subtitleText[index],
-                      style: const TextStyle(
-                        color: greyColor,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Text(
-                  introProvider.titleText[index],
-                  style: const TextStyle(
-                    color: wineColor,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  introProvider.goHome(context);
+                },
+                child: const Text(
+                  "Skip",
+                  style: TextStyle(
+                    color: greyColor,
+                    fontSize: 20,
                   ),
                 ),
               ),
-              Expanded(child: Container()),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: index == 0 ? 45 : 30,
-                          height: index == 0 ? 10 : 5,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: index == 0 ? wineColor : greyColor,
-                          ),
-                        ),
-                        Container(
-                          width: index == 1 ? 45 : 30,
-                          height: index == 1 ? 10 : 5,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: index == 1 ? wineColor : greyColor,
-                          ),
-                        ),
-                        Container(
-                          width: index == 2 ? 45 : 30,
-                          height: index == 2 ? 10 : 5,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: index == 2 ? wineColor : greyColor,
-                          ),
-                        ),
-                      ],
+            ),
+            const SizedBox(height: 20),
+            Flexible(
+              child: TabBarView(
+                controller: controller,
+                children: [
+                  BoardingItem(
+                    widget: introProvider.getImage(
+                      introProvider.svgIcons[0],
                     ),
-                    Button(
-                      title: index == 2 ? "Shop" : "Next",
-                      function: () {
-                        index == 2
-                            ? introProvider.goHome(context)
-                            : introPagecontroller.nextPage(
-                                duration: const Duration(seconds: 1),
-                                curve: Curves.easeIn,
-                              );
-                      },
+                    titleText: introProvider.titleText[0],
+                    subtitleText: introProvider.subtitleText[0],
+                  ),
+                  BoardingItem(
+                    widget: introProvider.getImage(
+                      introProvider.svgIcons[1],
                     ),
-                  ],
-                ),
+                    titleText: introProvider.titleText[1],
+                    subtitleText: introProvider.subtitleText[1],
+                  ),
+                  BoardingItem(
+                    widget: introProvider.getImage(
+                      introProvider.svgIcons[2],
+                    ),
+                    titleText: introProvider.titleText[2],
+                    subtitleText: introProvider.subtitleText[2],
+                  ),
+                ],
               ),
-            ],
-          ),
-        );
-      }),
-      itemCount: 3,
-    ));
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TabPageSelector(
+                  controller: controller,
+                  selectedColor: wineColor,
+                  color: Colors.grey.shade200,
+                  borderStyle: BorderStyle.none,
+                  indicatorSize: 16,
+                ),
+                const SizedBox(width: 40),
+                Button(
+                  title: controller.index == 2 ? "Home" : "Next",
+                  function: () {
+                    if (controller.index == 2) {
+                      introProvider.goHome(context);
+                    } else {
+                      controller.animateTo(
+                        controller.index + 1,
+                        curve: Curves.easeIn,
+                        duration: const Duration(seconds: 1),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 25)
+          ],
+        ),
+      ),
+    );
   }
 }
