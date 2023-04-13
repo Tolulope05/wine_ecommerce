574dd16ea2b4e195e1e2b934b2d94ada93046dfc
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-04-13 01:13:21 +0100

    added sample of cartview model from ephraimite

--- lib/view_model/cart_screen_model.dart
+++ lib/view_model/cart_screen_model.dart
@@ -8,4 +8,75 @@ class CartScreenModel with ChangeNotifier {
   void getToChekOutPage(BuildContext context) {
     _navigationServices.navigateToCheckOutPage(context);
   }
+
+  /**
+   * RxMap _items = {}.obs;
+
+  Map<String, ProductModel> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  int get totalAmount {
+    var total = 0;
+    _items.forEach(
+        (key, item) => total += (item.price! * item.cartItemQuantity as int));
+    return total;
+  }
+
+  void addItem(
+      String productID, double price, String productName, String image) {
+    if (_items.containsKey(productID)) {
+      _items.update(
+          productID,
+          (existingCartItem) => ProductModel(
+              productName: existingCartItem.productName,
+              productID: existingCartItem.productID,
+              price: existingCartItem.price,
+              imageUrl: existingCartItem.imageUrl,
+              cartItemQuantity: existingCartItem.cartItemQuantity! + 1));
+    } else {
+      _items.putIfAbsent(
+          productID,
+          () => ProductModel(
+              productID: productID,
+              productName: productName,
+              price: price,
+              imageUrl: image,
+              cartItemQuantity: 1));
+    }
+    Get.snackbar('Product Added', 'your product $productName has been addded',
+        duration: Duration(seconds: 3), snackPosition: SnackPosition.BOTTOM);
+  }
+
+  void removeItem(String productID) {
+    _items.remove(productID);
+  }
+
+  void removeSingleItem(String productID) {
+    if (!_items.containsKey(productID)) {
+      return;
+    }
+    if (_items[productID]!.cartItemQuantity! > 1) {
+      _items.update(
+          productID,
+          (existingCartItem) => ProductModel(
+                productID: existingCartItem.productID,
+                productName: existingCartItem.productName,
+                price: existingCartItem.price,
+                imageUrl: existingCartItem.imageUrl,
+                cartItemQuantity: -1,
+              ));
+    } else {
+      _items.remove(productID);
+    }
+  }
+
+  void clearCart() {
+    _items = {}.obs;
+  }
+   */
 }
